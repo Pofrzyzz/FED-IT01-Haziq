d2da3bb241f7f91fae5b3e285c5eac5315ab8fb4
Pofrzyzz <148027130+Pofrzyzz@users.noreply.github.com>
2023-12-01 08:58:42 +0800

    assg update

--- Assignment/Source Code/javascript.js
+++ Assignment/Source Code/javascript.js
@@ -4,14 +4,3 @@ function scrollToSection(sectionId) {
       section.scrollIntoView({ behavior: 'smooth' });
     }
   }
-
-  var clickableImage = document.getElementById('clickableImage');
-
-  // Add a click event listener
-  clickableImage.addEventListener('click', function() {
-
-    var url = 'https://open.spotify.com/album/78bpIziExqiI9qztvNFlQu?si=NttHx3ikRmKH_5E_ToTxzg';
-
-    // Open a new tab or window with the specified URL
-    window.open(url, '_blank');
-  });
