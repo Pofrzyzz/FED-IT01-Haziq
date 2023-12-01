f05101d8d60c347e74a23c68e11d893c6f232239
Pofrzyzz <148027130+Pofrzyzz@users.noreply.github.com>
2023-12-01 20:59:00 +0800

    Comment Updates

--- Assignment/Source Code/javascript.js
+++ Assignment/Source Code/javascript.js
@@ -1,6 +1,12 @@
+// // Function to scroll to a specific section on the webpage
 function scrollToSection(sectionId) {
-    var section = document.getElementById(sectionId);
-    if (section) {
+  // Get the DOM element corresponding to the provided sectionId
+  var section = document.getElementById(sectionId);
+
+  // Check if the section element exists in the DOM
+  if (section) {
+      // Scroll into view with smooth behavior
       section.scrollIntoView({ behavior: 'smooth' });
-    }
   }
+}
+
